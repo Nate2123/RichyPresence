ca31d4dc25e40305e390f326cf82d64a3f01b7a9
Nate2123 <73207168+Nate2123@users.noreply.github.com>
2021-10-30 15:39:45 +0800

    Update RichyPresence.cs

--- RichyPresence.cs
+++ RichyPresence.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +20,8 @@ namespace RichyPresence
         }
         public DiscordRpcClient client;
         bool initialized = false;
+    
+     
         private void button2_Click(object sender, EventArgs e)
         {
             initialized = true;
@@ -34,7 +36,7 @@ namespace RichyPresence
             {
                 MessageBox.Show("You need to initialize the app first!");
             }
-            else
+            if (textBox6.Text == "yes")
             {
                 client.SetPresence(new DiscordRPC.RichPresence()
                 {
@@ -49,8 +51,153 @@ namespace RichyPresence
                     }
                 });
             }
+            if (textBox6.Text == "no")
+            {
+                client.SetPresence(new DiscordRPC.RichPresence()
+                {
+                    Details = $"{textBox2.Text}",
+                    State = $"{textBox1.Text}",
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = $"{textBox3.Text}",
+                        LargeImageText = "Made by RichyPresence",
+                        SmallImageKey = $"{textBox4.Text}"
+                    }
+                });
+            }
+
         }
 
-        
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "State")
+            {
+                textBox1.Text = "";
+
+                textBox1.ForeColor = Color.Black;
+
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "State";
+
+                textBox1.ForeColor = Color.Silver;
+
+            }
+        }
+
+        private void textBox5_Enter(object sender, EventArgs e)
+        {
+            if (textBox5.Text == "App ID")
+            {
+                textBox5.Text = "";
+
+                textBox5.ForeColor = Color.Black;
+
+            }
+        }
+
+        private void textBox5_Leave(object sender, EventArgs e)
+        {
+            if (textBox5.Text == "")
+            {
+                textBox5.Text = "App ID";
+
+                textBox5.ForeColor = Color.Silver;
+
+            }
+        }
+
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "Large Image Key")
+            {
+                textBox3.Text = "";
+
+                textBox3.ForeColor = Color.Black;
+
+            }
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "")
+            {
+                textBox3.Text = "Large Image Key";
+
+                textBox3.ForeColor = Color.Silver;
+
+            }
+        }
+
+        private void textBox4_Enter(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "Small Image Key")
+            {
+                textBox4.Text = "";
+
+                textBox4.ForeColor = Color.Black;
+
+            }
+        }
+
+        private void textBox4_Leave(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "")
+            {
+                textBox4.Text = "Small Image Key";
+
+                textBox4.ForeColor = Color.Silver;
+
+            }
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Details")
+            {
+                textBox2.Text = "";
+
+                textBox2.ForeColor = Color.Black;
+
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "Details";
+
+                textBox2.ForeColor = Color.Silver;
+
+            }
+        }
+
+        private void textBox6_Enter(object sender, EventArgs e)
+        {
+            if (textBox6.Text == "Display Time (yes or no)")
+            {
+                textBox6.Text = "";
+
+                textBox6.ForeColor = Color.Black;
+
+            }
+        }
+
+        private void textBox6_Leave(object sender, EventArgs e)
+        {
+            if (textBox6.Text == "")
+            {
+                textBox6.Text = "Display Time (yes or no)";
+
+                textBox6.ForeColor = Color.Silver;
+
+            }
+        }
     }
 }
