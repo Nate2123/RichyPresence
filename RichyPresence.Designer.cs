ec334dacdbe8f5772bcf6d07756e1eecec9f50cb
Nate2123 <73207168+Nate2123@users.noreply.github.com>
2021-10-31 23:26:17 +0800

    Update RichyPresence.Designer.cs

--- RichyPresence.Designer.cs
+++ RichyPresence.Designer.cs
@@ -37,13 +37,15 @@ namespace RichyPresence
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.ForeColor = System.Drawing.Color.Silver;
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(12, 51);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(160, 20);
             this.textBox1.TabIndex = 0;
@@ -54,7 +56,7 @@ namespace RichyPresence
             // textBox2
             // 
             this.textBox2.ForeColor = System.Drawing.Color.Silver;
-            this.textBox2.Location = new System.Drawing.Point(12, 38);
+            this.textBox2.Location = new System.Drawing.Point(12, 77);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(160, 20);
             this.textBox2.TabIndex = 1;
@@ -65,7 +67,7 @@ namespace RichyPresence
             // textBox3
             // 
             this.textBox3.ForeColor = System.Drawing.Color.Silver;
-            this.textBox3.Location = new System.Drawing.Point(357, 12);
+            this.textBox3.Location = new System.Drawing.Point(12, 103);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(160, 20);
             this.textBox3.TabIndex = 2;
@@ -76,7 +78,7 @@ namespace RichyPresence
             // textBox4
             // 
             this.textBox4.ForeColor = System.Drawing.Color.Silver;
-            this.textBox4.Location = new System.Drawing.Point(357, 38);
+            this.textBox4.Location = new System.Drawing.Point(12, 129);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(160, 20);
             this.textBox4.TabIndex = 3;
@@ -87,7 +89,7 @@ namespace RichyPresence
             // textBox5
             // 
             this.textBox5.ForeColor = System.Drawing.Color.Silver;
-            this.textBox5.Location = new System.Drawing.Point(178, 12);
+            this.textBox5.Location = new System.Drawing.Point(12, 12);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(173, 20);
             this.textBox5.TabIndex = 4;
@@ -97,7 +99,7 @@ namespace RichyPresence
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(178, 64);
+            this.button1.Location = new System.Drawing.Point(191, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 20);
             this.button1.TabIndex = 5;
@@ -107,7 +109,7 @@ namespace RichyPresence
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(276, 64);
+            this.button2.Location = new System.Drawing.Point(272, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 20);
             this.button2.TabIndex = 6;
@@ -115,24 +117,47 @@ namespace RichyPresence
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(353, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 20);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Turn off";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // textBox6
             // 
             this.textBox6.ForeColor = System.Drawing.Color.Silver;
-            this.textBox6.Location = new System.Drawing.Point(178, 38);
+            this.textBox6.Location = new System.Drawing.Point(12, 155);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(173, 20);
-            this.textBox6.TabIndex = 7;
-            this.textBox6.Text = "Display Time (yes or no)";
+            this.textBox6.Size = new System.Drawing.Size(160, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "Button Text";
             this.textBox6.Enter += new System.EventHandler(this.textBox6_Enter);
             this.textBox6.Leave += new System.EventHandler(this.textBox6_Leave);
+            // 
+            // textBox7
+            // 
+            this.textBox7.ForeColor = System.Drawing.Color.Silver;
+            this.textBox7.Location = new System.Drawing.Point(12, 181);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(160, 20);
+            this.textBox7.TabIndex = 10;
+            this.textBox7.Text = "Button URL";
+            this.textBox7.Enter += new System.EventHandler(this.textBox7_Enter);
+            this.textBox7.Leave += new System.EventHandler(this.textBox7_Leave);
             // 
             // RichyPresence
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(529, 104);
+            this.ClientSize = new System.Drawing.Size(434, 235);
+            this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox5);
@@ -159,7 +184,9 @@ namespace RichyPresence
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
     }
 }
 
